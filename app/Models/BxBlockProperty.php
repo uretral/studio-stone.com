a599f7bc9229408d9d6b5509e015888aa6dd5da7
murindev <cdnadom@gmail.com>
2024-03-14 22:13:36 +0300

    first commit

---
+++ app/Models/BxBlockProperty.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BxBlockProperty extends Model
+{
+    use HasFactory;
+    protected $table = 'b_iblock_iproperty';
+}
